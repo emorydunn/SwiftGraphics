f86afe3de188547ad4e7cf46f7403820b9c4d853
Emory Dunn <edunn@emorydunn.com>
2020-05-22 14:52:33 -0700

    Refactor SVG drawing to work with styles

--- Sources/SwiftGraphics/Drawing/SVG Drawing/SVGContext.swift
+++ Sources/SwiftGraphics/Drawing/SVG Drawing/SVGContext.swift
@@ -9,14 +9,33 @@ import Foundation
 
 public class SVGContext: DrawingContext {
     
-    public var width: Int
-    public var height: Int
-    
-    public var shapes: [SVGDrawable] = []
+    public var svg: XMLElement
+    public let width: Int
+    public let height: Int
     
     public init(width: Int, height: Int) {
         self.width = width
         self.height = height
+        
+        // Set up the SVG root element
+        self.svg = XMLElement(kind: .element)
+        svg.name = "svg"
+        svg.addAttribute(width, forKey: "width")
+        svg.addAttribute(height, forKey: "height")
+        svg.addAttribute("http://www.w3.org/2000/svg", forKey: "xmlns")
+        
+        // Transform the shapes into XML elements and add them to the root
+        //        let nodes = shapes.map {
+        //            $0.svgElement()
+        //        }
+        //        svgRoot.setChildren(nodes)
+        
+//        self.svg = XMLDocument(kind: .document)
+//        svg.setRootElement(svgRoot)
+//        svg.characterEncoding = "UTF-8"
+//        svg.isStandalone = false
+//        svg.documentContentKind = .xml
+//        svg.version = "1.0"
     }
     
     public convenience init(sketch: SketchView) {
@@ -28,33 +47,26 @@ public class SVGContext: DrawingContext {
     
     
     public func addShape(_ shape: SVGDrawable) {
-        shapes.append(shape)
+        let xml = shape.svgElement()
+        
+        svg.addChild(xml)
+//        shapes.append(shape)
     }
     
     public func makeDoc() -> XMLDocument {
+//
         
-        // Set up the SVG root element
-        let svgRoot = XMLElement(kind: .element)
-        svgRoot.name = "svg"
-        svgRoot.addAttribute(width, forKey: "width")
-        svgRoot.addAttribute(height, forKey: "height")
-        svgRoot.addAttribute("http://www.w3.org/2000/svg", forKey: "xmlns")
-        
-        // Transform the shapes into XML elements and add them to the root
-        let nodes = shapes.map {
-            $0.svgElement()
-        }
-        svgRoot.setChildren(nodes)
-        
+//        svg.setChildren(nodes)
+
         let doc = XMLDocument(kind: .document)
-        doc.setRootElement(svgRoot)
+        doc.setRootElement(svg)
         doc.characterEncoding = "UTF-8"
         doc.isStandalone = false
         doc.documentContentKind = .xml
         doc.version = "1.0"
-        
+
         return doc
     }
-    
+//
 }
 
