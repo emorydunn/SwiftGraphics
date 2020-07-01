849b343bd48e84124186fbbe6da1e96665ef66fe
Emory Dunn <emory@emorydunn.com>
2020-07-01 11:46:07 -0700

    Remove Intersectable

--- Sources/SwiftGraphics/Base Protocols/Polygon.swift
+++ Sources/SwiftGraphics/Base Protocols/Polygon.swift
@@ -13,19 +13,5 @@ public protocol Polygon: Intersectable {
     
     func contains(_ point: Vector) -> Bool
     
-    func rayIntersection(origin: Vector, theta: Radians) -> Vector?
-    
     func rayIntersection(_ theta: Radians) -> Vector
-}
-
-
-extension Polygon {
-    
-    /// Return an array of points at the specified angular distance apart
-    /// - Parameter angle: Angle in degrees
-    public func pointsDistributed(every angle: Degrees, starting: Degrees = 0, ending: Degrees = 360) -> [Vector] {
-        stride(from: starting, to: ending, by: angle).map { angle in
-            rayIntersection(angle.toRadians())
-        }
-    }
 }
