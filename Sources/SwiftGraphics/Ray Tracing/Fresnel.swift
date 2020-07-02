77ccacee3d26e52dbcf851149c83921e04d83bbf
Emory Dunn <emory@emorydunn.com>
2020-07-02 15:22:43 -0700

    Switch over to new method

--- Sources/SwiftGraphics/Ray Tracing/Fresnel.swift
+++ Sources/SwiftGraphics/Ray Tracing/Fresnel.swift
@@ -25,7 +25,7 @@ public class Fresnel: Line, RayTracer {
     ///   - origin: The origin of the ray being cast
     ///   - bb: The `BoundingBox` to terminate the ray on
     ///   - objects: Objects to test for intersection, sorted by distance from the origin
-    public func intersections(for angle: Radians, origin: Vector, bb: BoundingBox, objects: [Intersectable]) -> [Line] {
+    public func intersections(for angle: Radians, origin: Vector, objects: [Intersectable]) -> [Line] {
         
         // Make a mutable copy
         var angle = angle
@@ -33,15 +33,9 @@ public class Fresnel: Line, RayTracer {
         // Set angle to the normal angle
         angle = reflectionAngle.toRadians() + normal().heading()
         
-        return defaultIntersections(for: angle, origin: origin, bb: bb, objects: objects)
+        return defaultIntersections(for: angle, origin: origin, objects: objects)
 
         
     }
-    
-    public func intersections(for angle: Radians, origin: Vector, objects: [Intersectable]) -> [Line] {
-        return []
-    }
-    
-    
     
 }
