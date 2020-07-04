d05bfc4536a2a0c7aeabbf79192f5f60753153e9
Emory Dunn <emory@emorydunn.com>
2020-07-04 11:52:51 -0700

    Add `LinearEmitter`

---
+++ Sources/SwiftGraphics/Ray Tracing/LinearEmitter.swift
@@ -0,0 +1,69 @@
+//
+//  LinearEmitter.swift
+//  SwiftGraphics
+//
+//  Created by Emory Dunn on 7/4/20.
+//  Copyright © 2020 Lost Cause Photographic, LLC. All rights reserved.
+//
+
+import Foundation
+
+public class LinearEmitter: Line, Emitter {
+    
+    public var style: RayTraceStyle = .line
+    
+    public var rayStep: Double
+    
+    /// Instantiate a new `Line`
+    /// - Parameters:
+    ///   - start: Starting point
+    ///   - end: Ending point
+    public init(_ start: Vector, _ end: Vector, rayStep: Double) {
+        self.rayStep = rayStep
+        super.init(start, end)
+    }
+    
+    /// Instantiate a new `Line` from coordinates
+    /// - Parameters:
+    ///   - x1: Starting X coordinate
+    ///   - y1: Starting Y coordinate
+    ///   - x2: Ending X coordinate
+    ///   - y2: Ending Y coordinate
+    public init(_ x1: Double, _ y1: Double, _ x2: Double, _ y2: Double, rayStep: Double) {
+        self.rayStep = rayStep
+        super.init(x1, y1, x2, y2)
+    }
+    
+    
+    public func draw(objects: [Intersectable]) {
+        // Draw the circle
+        if case .line = style {
+            super.draw()
+        }
+        
+        
+        guard rayStep > 0 else { return }
+        
+        let angleVector: Vector = normal()
+        angleVector.rotate(by: 180.toRadians())
+        let angle = angleVector.heading()
+        let percentStep = (rayStep / length)
+        
+        stride(from: 0, to: 1 + percentStep, by: percentStep).forEach { percent in
+            let origin = start + ((end - start) * percent)
+//            origin.debugDraw(in: SwiftGraphicsContext.current as! CGContext)
+            
+            let intersections = self.intersections(
+                for: angle,
+                origin: origin,
+                objects: objects)
+
+            drawIntersections(intersections)
+            
+        }
+
+    }
+    
+    
+
+}
