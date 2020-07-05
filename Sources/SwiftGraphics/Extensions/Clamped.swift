77ae6dc988e4bca0bbef6ce57150be929579ee0d
Emory Dunn <emory@emorydunn.com>
2020-07-05 16:13:15 -0700

    Mark public

--- Sources/SwiftGraphics/Extensions/Clamped.swift
+++ Sources/SwiftGraphics/Extensions/Clamped.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 extension Comparable {
-    func clamped(to limits: ClosedRange<Self>) -> Self {
+    public func clamped(to limits: ClosedRange<Self>) -> Self {
         return min(max(self, limits.lowerBound), limits.upperBound)
     }
 }
 
 extension Strideable where Stride: SignedInteger {
-    func clamped(to limits: CountableClosedRange<Self>) -> Self {
+    public func clamped(to limits: CountableClosedRange<Self>) -> Self {
         return min(max(self, limits.lowerBound), limits.upperBound)
     }
 }
