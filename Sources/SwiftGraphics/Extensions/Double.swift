bbd3c785672fda7cf3075bf3d6e60bb3f7b26517
Emory Dunn <edunn@emorydunn.com>
2020-07-27 15:26:05 -0700

    Deprecate `Math` struct

---
+++ Sources/SwiftGraphics/Extensions/Double.swift
@@ -0,0 +1,28 @@
+//
+//  Double.swift
+//  
+//
+//  Created by Emory Dunn on 7/27/20.
+//
+
+import Foundation
+
+extension Double {
+    
+    /// Return a new Double that has been squared
+    public func squared() -> Double {
+        return pow(self, 2)
+    }
+    
+    /// Square the receiver
+    public mutating func square() {
+        self = pow(self, 2)
+    }
+    
+    /// Negate the receiver if it is less that the specified number.
+    mutating func negate(ifLessThan num: Double) {
+        if self < num {
+            self.negate()
+        }
+    }
+}
