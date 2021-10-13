03860679ea98c9e4f34cdd7f1ff95020d73a45a3
Emory Dunn <emory@emorydunn.com>
2021-10-12 17:47:03 -0700

    Add new init

--- Sources/SwiftGraphics2/Polygon/Rectangle.swift
+++ Sources/SwiftGraphics2/Polygon/Rectangle.swift
@@ -8,36 +8,51 @@
 import Foundation
 import simd
 
+/// A rectangle defined by its center, width, and height.
 public struct Rectangle: Polygon {
     
+    /// The center of the rectangle
     public var origin: Vector {
         didSet {
             self.points = makePolygon()
         }
     }
     
-    public var height: Double {
-        didSet {
-            self.points = makePolygon()
-        }
-    }
-    
+    /// The width of the rectangle
     public var width: Double {
         didSet {
             self.points = makePolygon()
         }
     }
     
+    /// The height of the rectangle
+    public var height: Double {
+        didSet {
+            self.points = makePolygon()
+        }
+    }
+    
+    /// The angle of rotation of the rectangle
     public var rotation: Angle {
         didSet {
             self.points = makePolygon()
         }
     }
     
+    /// The points that make up the rectangle
     public private(set) var points: [Vector] = []
 
-    public init(origin: Vector, height: Double, width: Double, rotation: Angle = .degrees(0)) {
-        self.origin = origin
+    public init(center: Vector, width: Double, height: Double, rotation: Angle = .degrees(0)) {
+        self.origin = center
+        self.height = height
+        self.width = width
+        self.rotation = rotation
+        
+        self.points = makePolygon()
+    }
+    
+    public init(centerX x: Double, y: Double, width: Double, height: Double, rotation: Angle = .degrees(0)) {
+        self.origin = Vector(x, y)
         self.height = height
         self.width = width
         self.rotation = rotation
@@ -60,11 +75,20 @@ public struct Rectangle: Polygon {
         let rotMatrix = MatrixTransformation.rotate(by: rotation)
         let compoundMatrix = transMatrix * rotMatrix
         
+        // Center rectangle
         return [
             Vector(-width / 2,  height / 2, transformation: compoundMatrix),
             Vector( width / 2,  height / 2, transformation: compoundMatrix),
             Vector( width / 2, -height / 2, transformation: compoundMatrix),
             Vector(-width / 2, -height / 2, transformation: compoundMatrix),
         ]
+        
+        // Corner rectangle
+//        return [
+//            Vector(0,  0, transformation: compoundMatrix),
+//            Vector(width,  0, transformation: compoundMatrix),
+//            Vector(width, height, transformation: compoundMatrix),
+//            Vector(0, height, transformation: compoundMatrix),
+//        ]
     }
 }
