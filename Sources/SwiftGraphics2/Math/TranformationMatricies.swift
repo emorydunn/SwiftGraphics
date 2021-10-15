6dd1416d50f2f89e41401a613e517db40ddca35a
Emory Dunn <emory@emorydunn.com>
2021-10-15 15:32:54 -0700

    Add docstrings

--- Sources/SwiftGraphics2/Math/TranformationMatricies.swift
+++ Sources/SwiftGraphics2/Math/TranformationMatricies.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  TranformationMatricies.swift
 //  
 //
 //  Created by Emory Dunn on 10/11/21.
@@ -9,6 +9,10 @@ import Foundation
 import simd
 
 public enum MatrixTransformation {
+    
+    /// Create a transformation matrix to rotate a vector.
+    /// - Parameter angle: The angle of rotation.
+    /// - Returns: A rotation matrix.
     public static func rotate(by angle: Angle) -> simd_double3x3 {
         let rows = [
             simd_double3( cos(angle.radians), sin(angle.radians), 0),
@@ -19,6 +23,10 @@ public enum MatrixTransformation {
         return double3x3(rows: rows)
     }
     
+    /// Create a transformation matrix to translate a vector.
+    /// - Parameter x: The amount to move in the x axis.
+    /// - Parameter y: The amount to move in the y axis.
+    /// - Returns: A translation matrix.
     public static func translate(x: Double, y: Double) -> simd_double3x3 {
         var matrix = matrix_identity_double3x3
             
@@ -28,6 +36,9 @@ public enum MatrixTransformation {
         return matrix
     }
     
+    /// Create a transformation matrix to translate a vector.
+    /// - Parameter vector: A Vector describing the translation.
+    /// - Returns: A translation matrix.
     public static func translate(vector: Vector) -> simd_double3x3 {
         translate(x: vector.x, y: vector.y)
     }
