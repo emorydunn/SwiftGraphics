2f1daae7ba678b539d6a4f0b4dbd3680491dbcec
Emory Dunn <emory@emorydunn.com>
2022-09-06 14:23:33 -0700

    Disable the SwiftGraphics library

--- Package.swift
+++ Package.swift
@@ -8,13 +8,13 @@ let package = Package(
     platforms: [.macOS(.v10_15)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "SwiftGraphics",
-            targets: ["SwiftGraphics"]),
-        
-        .library(
-            name: "SwiftGraphics2",
-            targets: ["SwiftGraphics2"]),
+//        .library(
+//            name: "SwiftGraphics",
+//            targets: ["SwiftGraphics"]),
+//
+//        .library(
+//            name: "SwiftGraphics2",
+//            targets: ["SwiftGraphics2"]),
 
 		.library(
 			name: "SilicaRendering",
@@ -33,18 +33,16 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in
         // packages which this package depends on.
-        .target(
-            name: "SwiftGraphics",
-            dependencies: []),
-        .testTarget(
-            name: "SwiftGraphicsTests",
-            dependencies: ["SwiftGraphics"]),
+//        .target(
+//            name: "SwiftGraphics",
+//            dependencies: []),
+//        .testTarget(
+//            name: "SwiftGraphicsTests",
+//            dependencies: ["SwiftGraphics"]),
         
         .target(
             name: "SwiftGraphics2",
-            dependencies: [
-//				"Silica"
-			]),
+            dependencies: []),
         .testTarget(
             name: "SwiftGraphics2Tests",
             dependencies: ["SwiftGraphics2"]),
