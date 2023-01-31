a0c666b2b565149056be8d34ba825819c7867acc
Emory Dunn <emory@emorydunn.com>
2023-01-31 13:40:57 -0800

    Add a method for making the rect

--- Sources/CoreGraphicsRendering/Shapes/Rectangle.swift
+++ Sources/CoreGraphicsRendering/Shapes/Rectangle.swift
@@ -10,12 +10,18 @@ import CoreGraphics
 import SwiftGraphics2
 
 extension Rectangle: CGDrawable {
-	public func draw(in context: CGContext) {
 
+	public func makeCGRect() -> CGRect {
 		// Create the rectangle
 		let transMatrix = MatrixTransformation.translate(vector: origin)
 		let corner = Vector(-width / 2,  -height / 2, transformation: transMatrix)
-		let rect = CGRect(x: corner.x, y: corner.y, width: width, height: height)
+		return CGRect(x: corner.x, y: corner.y, width: width, height: height)
+	}
+
+	public func draw(in context: CGContext) {
+
+		// Create the rectangle
+		let rect = makeCGRect()
 
 		// Save the state and rotate
 		context.saveGState()
