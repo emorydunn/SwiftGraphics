27c2c6823b5a85fb94083f65bae8f032b5fa9727
Emory Dunn <emory@emorydunn.com>
2025-12-26 08:49:02 -0800

    Add `Stridable` to `Angle`

--- Sources/SwiftGraphics2/Math/Angle.swift
+++ Sources/SwiftGraphics2/Math/Angle.swift
@@ -9,13 +9,19 @@ import Foundation
 
 /// Represents an angle, both in degrees and radians.
 public struct Angle {
-    
+
     /// Create a new Angle from degrees.
     /// - Parameter value: The angle in degrees.
     public static func degrees(_ value: Double) -> Angle {
         Angle(degrees: value)
     }
-    
+
+	/// Create a new Angle from degrees.
+	/// - Parameter value: The angle in degrees.
+	public static func degrees(_ value: Int) -> Angle {
+		Angle(degrees: Double(value))
+	}
+
     /// Create a new Angle from radians.
     /// - Parameter value: The angle in radians.
     public static func radians(_ value: Double) -> Angle {
@@ -30,37 +36,54 @@ public struct Angle {
     
     /// Create a new Angle from degrees.
     /// - Parameter degrees: The angle in degrees.
-    init(degrees: Double) {
+    public init(degrees: Double) {
         self.degrees = degrees
         self.radians = degrees * Double.pi / 180
     }
     
     /// Create a new Angle from degrees.
     /// - Parameter radians: The angle in degrees.
-    init(radians: Double) {
+	public init(radians: Double) {
         self.degrees = radians * 180 / Double.pi
         self.radians = radians
     }
     
 }
 
-public extension Angle {
-    
-    /// Add two angles together.
-    /// - Returns: The sum of the two angles.
-    static func + (lhs: Angle, rhs: Angle) -> Angle {
-        Angle(radians: lhs.radians + rhs.radians)
-    }
-    
-    /// Add two angles together.
-    /// - Returns: The difference of the two angles.
-    static func - (lhs: Angle, rhs: Angle) -> Angle {
-        Angle(radians: lhs.radians - rhs.radians)
-    }
+extension Angle: Comparable, Equatable {
+	public static func == (lhs: Self, rhs: Self) -> Bool {
+		lhs.radians == rhs.radians
+	}
+
+	public static func < (lhs: Self, rhs: Self) -> Bool {
+		lhs.radians < rhs.radians
+	}
 }
 
 public extension Angle {
-    
+
+	/// Add two angles together.
+	/// - Returns: The sum of the two angles.
+	static func + (lhs: Angle, rhs: Angle) -> Angle {
+		Angle(degrees: lhs.degrees + rhs.degrees)
+	}
+
+	/// Add two angles together.
+	/// - Returns: The difference of the two angles.
+	static func - (lhs: Angle, rhs: Angle) -> Angle {
+		Angle(degrees: lhs.degrees - rhs.degrees)
+	}
+
+	public static func * (lhs: Angle, rhs: Angle) -> Angle {
+		Angle(degrees: lhs.degrees * rhs.degrees)
+	}
+
+	public static func *= (lhs: inout Angle, rhs: Angle) {
+		lhs = Angle(degrees: lhs.degrees * rhs.degrees)
+	}
+}
+
+public extension Angle {
     /// Pi
     static let pi = Angle(radians: Double.pi)
     
@@ -79,4 +102,40 @@ public extension Angle {
     
     /// A quarter circle
     static let quarterCircle = Angle.halfPi
+}
+
+extension Angle: SignedNumeric {
+	public init?<T>(exactly source: T) where T : BinaryInteger {
+		self = .degrees(Double(source))
+	}
+
+	public var magnitude: Double { abs(degrees) }
+
+	public init(integerLiteral value: Int) {
+		self = .degrees(Double(value))
+	}
+
+	public mutating func negate() {
+		self = Angle(degrees: -degrees)
+	}
+
+	public func negated() -> Angle {
+		Angle(degrees: -degrees)
+	}
+}
+
+extension Angle: Strideable {
+	public func advanced(by n: Angle) -> Angle {
+		self + n
+	}
+
+	public func distance(to other: Angle) -> Angle {
+		other - self
+	}
+}
+
+extension Angle: CustomStringConvertible {
+	public var description: String {
+		"\(degrees)º"
+	}
 }
