11b7147f1c85af92f797a8f3d9312ea252be5c1c
Emory Dunn <emory@emorydunn.com>
2025-12-30 13:41:35 -0800

    Group emitters in builder

--- Sources/SwiftGraphics2/RayTracing/EmitterBuilder.swift
+++ Sources/SwiftGraphics2/RayTracing/EmitterBuilder.swift
@@ -19,32 +19,58 @@ public struct EmitterBuilder {
 		content()
 	}
 
-	public static func buildBlock(_ content: Emitter...) -> [Emitter] {
-		content
+	public static func buildBlock(_ content: Emitter...) -> Emitter {
+		if content.count == 1 {
+			content[0]
+		} else {
+			EmitterGroup(content)
+		}
 	}
 
-	public static func buildBlock<Content: Emitter>(_ content: Content...) -> [Content] {
-		content
-	}
-
-	public static func buildArray(_ content: [Emitter]) -> [Emitter] {
-		content
+	public static func buildArray(_ content: [Emitter]) -> Emitter {
+		EmitterGroup(content)
 	}
 
 	public static func buildOptional(_ component: Emitter?) -> Emitter? {
 		component
 	}
 
-	public static func buildEither(first component: Emitter) -> [Emitter] {
-		[
-			component
-		]
+	public static func buildEither(first component: Emitter) -> Emitter {
+		component
 	}
 
-	public static func buildEither(second component: Emitter) -> [Emitter] {
-		[
-			component
-		]
+	public static func buildEither(second component: Emitter) -> Emitter {
+		component
+	}
+
+	public static func buildFinalResult(_ component: Emitter) -> [Emitter] {
+		switch component {
+		case let e as EmitterGroup:
+			e.emitters
+		default:
+			[component]
+		}
+	}
+
+}
+
+public struct EmitterGroup: Emitter, CustomStringConvertible {
+	let emitters: [Emitter]
+
+	public var style: RayTraceStyle = .line
+
+	public var description: String {
+		"Emitter Group \(emitters.count)"
+	}
+
+	init(_ emitters: [Emitter]) {
+		self.emitters = emitters
+	}
+
+	public mutating func run(objects: [any RayTracable], initialIndex: Double) {
+		for var emitter in emitters {
+			emitter.run(objects: objects, initialIndex: initialIndex)
+		}
 	}
 
 }
