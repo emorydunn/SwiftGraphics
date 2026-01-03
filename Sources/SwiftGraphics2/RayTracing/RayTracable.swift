baeba7f23cbe43d3730a14bb0e18f85c570d6fae
Emory Dunn <emory@emorydunn.com>
2026-01-03 09:14:35 -0800

    Remove `Line` interfaces

--- Sources/SwiftGraphics2/RayTracing/RayTracable.swift
+++ Sources/SwiftGraphics2/RayTracing/RayTracable.swift
@@ -8,48 +8,32 @@ import Foundation
 
 /// A shape that can interact with a `Ray`
 public protocol RayTracable {
-    
-    /// Return the intersection of a ray
-    /// - Parameters:
-    ///   - ray: The `Ray` to intersect
-    func rayIntersection(_ ray: Ray) -> Vector?
+
+	/// Return the intersection of a ray
+	/// - Parameters:
+	///   - ray: The `Ray` to intersect
+	func rayIntersection(_ ray: Ray) -> Vector?
 
 	func rayIntersectionDistance(_ ray: Ray) -> Double?
 
-    /// Modify the path of a ray.
-    ///
-    /// The default implementation of this method terminates the `Ray`.
-    /// - Parameter ray: The ray to modify
-    func modifyRay(_ ray: Ray)
+	/// Modify the path of a ray.
+	///
+	/// The default implementation of this method terminates the `Ray`.
+	/// - Parameter ray: The ray to modify
+	func modifyRay(_ ray: Ray)
 
-	func interface(of intersection: Vector) -> Line
+	func interface(of intersection: Vector) -> Vector
+
+	func deflectionAngle(for ray: Ray, at interface: Line, index1: Double, index2: Double) -> Vector
 }
 
 public extension RayTracable {
-    
-    /// Modify the path of a ray.
-    ///
-    /// The default implementation of this method terminates the `Ray`.
-    func modifyRay(_ ray: Ray) {
-        ray.terminateRay()
-    }
 
-	/// Calculate the angle of deflection using Snell's Law of Reflection
-	/// - Parameters:
-	///   - dir: The angle of intersection
-	///   - interface: The interface
-	///   - refraction: The refraction index of the material
-	///   - extIndex: The refraction index of the exterior material
-	/// - Returns: A `Vector` rotated by the angle of reflection
-	func deflectionAngle(for dir: Vector, at interface: Line, refraction: Double = 1.46, extIndex: Double = 1.0) -> Vector {
-
-		// Determine the angle from the normal
-		let thetaInc = dir.angleBetween(interface.normal()).radians
-
-		// Snell's Law of reflection
-		let deflection = asin((extIndex * sin(thetaInc)) / refraction)
-
-		return dir.rotated(by: .radians(deflection))
+	/// Modify the path of a ray.
+	///
+	/// The default implementation of this method terminates the `Ray`.
+	func modifyRay(_ ray: Ray) {
+		ray.terminateRay()
 	}
 
 	/// Calculate the angle of deflection using Snell's Law of Reflection
@@ -59,18 +43,13 @@ public extension RayTracable {
 	///   - refraction: The refraction index of the material
 	///   - extIndex: The refraction index of the exterior material
 	/// - Returns: A `Vector` rotated by the angle of reflection
-	func deflectionAngle(for dir: Vector, at interface: Line, index1: Double, index2: Double) -> Vector {
-
-		// Determine the angle from the normal
-		let thetaInc = dir.angleBetween(interface.normal()).radians
-
-		// Snell's Law of reflection
-		let deflection = asin((index1 / index2) * sin(thetaInc))
-
-		return dir.rotated(by: .radians(deflection))
+	func deflectionAngle(for ray: Ray, at interface: Vector, index1: Double, index2: Double) -> Angle {
+		deflectionAngle(for: ray, at: interface, index1: index1, index2: index2).heading()
 	}
 
-	/// Calculate the angle of deflection using Snell's Law of Reflection
+	/// Calculate the angle of deflection using Snell's Law of Reflection.
+	///
+	/// Adapted from _Reflections and Refractions in Ray Tracing_ by Bram de Greve.
 	/// - Parameters:
 	///   - dir: The angle of intersection
 	///   - interface: The interface
@@ -92,24 +71,17 @@ public extension RayTracable {
 
 		return directionNormalized * relativeIOR + interface * (relativeIOR * cosAngleIn - sqrt(1 - sinSqrAngle))
 	}
-
-	/// Calculate the angle of deflection using Snell's Law of Reflection
+	
+	/// Calculate the critical angle at which a ray will be reflected rather than refracted.
 	/// - Parameters:
-	///   - dir: The angle of intersection
-	///   - interface: The interface
-	///   - refraction: The refraction index of the material
-	///   - extIndex: The refraction index of the exterior material
-	/// - Returns: A `Vector` rotated by the angle of reflection
-	func deflectionAngle(for ray: Ray, at interface: Line, index1: Double, index2: Double) -> Angle {
-		deflectionAngle(for: ray, at: interface, index1: index1, index2: index2).heading()
-	}
-
-	func criticalAngle(for dir: Vector, at interface: Line, index1: Double, index2: Double) -> Angle {
+	///   - index1: The index of refraction of the first material.
+	///   - index2: The index of refraction of the second material.
+	func criticalAngle(index1: Double, index2: Double) -> Angle {
 		let thetaCrit = asin(index2 / index1)
 
 		return .radians(thetaCrit)
 	}
-	
+
 	/// Calculate the ratio of light reflected versus transmitted through an interface.
 	///
 	/// The higher the returned value the more light is reflected versus transmitted.
@@ -137,41 +109,26 @@ public extension RayTracable {
 }
 
 public extension RayTracable where Self: ClosedShape {
-    
-    /// The interface of the intersection of a ray
-    /// - Parameter intersection: The point of intersection
-    /// - Returns: A line representing the normal
-	func interface(of intersection: Vector) -> Line {
+
+	/// The interface of the intersection of a ray
+	/// - Parameter intersection: The point of intersection
+	/// - Returns: A line representing the normal
+	func interface(of intersection: Vector) -> Vector {
 		var entryAngle = angle(ofPoint: intersection)
 		if entryAngle.radians < 0 {
 			entryAngle = Angle.fullCircle + entryAngle
 		}
 
-		let tangentAngle = entryAngle + Angle.quarterCircle
-
-		let interface = Line(center: intersection, direction: tangentAngle, length: 50)
-
-		if intersection.isBehind(line: interface) {
-			return interface
-		} else {
-			return interface.pointsSwitched()
-		}
+		return Vector(angle: entryAngle + Angle.quarterCircle)
 	}
-    
-    /// Deflect a ray according to Snell's Law
-    /// - Parameter ray: The ray to deflect
-    func deflectRay(_ ray: Ray) {
-        let interface = self.interface(of: ray.origin)
-        ray.direction = deflectionAngle(for: ray.direction, at: interface)
-    }
-    
-    /// Calculate the critical angle of a material
-    /// - Parameters:
-    ///   - refraction: The refraction index of the material
-    ///   - extIndex: The refraction index of the exterior material
-    /// - Returns: The critical angle
-    func criticalAngle(refraction: Double, extIndex: Double) -> Angle {
+
+	/// Calculate the critical angle of a material
+	/// - Parameters:
+	///   - refraction: The refraction index of the material
+	///   - extIndex: The refraction index of the exterior material
+	/// - Returns: The critical angle
+	func criticalAngle(refraction: Double, extIndex: Double) -> Angle {
 		return Angle.radians(asin(extIndex / refraction))
-    }
+	}
 }
 
