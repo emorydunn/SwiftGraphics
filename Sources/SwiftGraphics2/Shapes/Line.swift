5df3e60e57c878ff78b1bd90c5ed827d00a791c3
Emory Dunn <emory@emorydunn.com>
2026-01-03 09:17:31 -0800

    Update Line tracing

--- Sources/SwiftGraphics2/Shapes/Line.swift
+++ Sources/SwiftGraphics2/Shapes/Line.swift
@@ -1,6 +1,6 @@
 //
 //  Line.swift
-//  
+//
 //
 //  Created by Emory Dunn on 10/13/21.
 //
@@ -8,38 +8,38 @@
 import Foundation
 
 public struct Line: Shape, Drawable {
-    
-    /// The starting point of the line
-    public var start: Vector
 
-    /// The ending point of the line
-    public var end: Vector
-    
-    public var length: Double { end.distance(to: start) }
-    
-    /// The midpoint of the line
-    public var center: Vector {
-        Vector(
-            (end.x + start.x) / 2,
-            (end.y + start.y) / 2
-        )
-    }
-    
-    public init(start: Vector, end: Vector) {
-        self.start = start
-        self.end = end
-    }
-    
-    /// Instantiate a new `Line` from coordinates
-    /// - Parameters:
-    ///   - x1: Starting X coordinate
-    ///   - y1: Starting Y coordinate
-    ///   - x2: Ending X coordinate
-    ///   - y2: Ending Y coordinate
-    public init(_ x1: Double, _ y1: Double, _ x2: Double, _ y2: Double) {
-        self.start = Vector(x1, y1)
-        self.end = Vector(x2, y2)
-    }
+	/// The starting point of the line
+	public var start: Vector
+
+	/// The ending point of the line
+	public var end: Vector
+
+	public var length: Double { end.distance(to: start) }
+
+	/// The midpoint of the line
+	public var center: Vector {
+		Vector(
+			(end.x + start.x) / 2,
+			(end.y + start.y) / 2
+		)
+	}
+
+	public init(start: Vector, end: Vector) {
+		self.start = start
+		self.end = end
+	}
+
+	/// Instantiate a new `Line` from coordinates
+	/// - Parameters:
+	///   - x1: Starting X coordinate
+	///   - y1: Starting Y coordinate
+	///   - x2: Ending X coordinate
+	///   - y2: Ending Y coordinate
+	public init(_ x1: Double, _ y1: Double, _ x2: Double, _ y2: Double) {
+		self.start = Vector(x1, y1)
+		self.end = Vector(x2, y2)
+	}
 
 	/// Instantiate a new `Line` from a center point
 	/// - Parameters:
@@ -79,67 +79,67 @@ public struct Line: Shape, Drawable {
 		self.init(origin: origin, direction: direction.heading(), length: length)
 	}
 
-    /// Determine whether a point is on the line
-    ///
-    /// From https://gamedev.stackexchange.com/a/57746
-    ///
-    /// - Parameter point: Whether the point is on the line
-    public func contains(_ point: Vector) -> Bool {
+	/// Determine whether a point is on the line
+	///
+	/// From https://gamedev.stackexchange.com/a/57746
+	///
+	/// - Parameter point: Whether the point is on the line
+	public func contains(_ point: Vector) -> Bool {
 		start.cross(point).sign() == point.cross(end).sign()
-    }
-    
-    /// Calculate the vector normal of the line
-    ///
-    /// - Returns: A `Vector` whose heading is perpendicular to the line
-    public func normal() -> Vector {
-        //calculate base top normal
-        let baseDelta = (end - start).normalized()
-        let normal = Vector(-baseDelta.y, baseDelta.x)
+	}
 
-        return normal
-    }
-    
-    public func slope() -> Double {
-        return (end.y - start.y) / (end.x - start.x)
-    }
+	/// Calculate the vector normal of the line
+	///
+	/// - Returns: A `Vector` whose heading is perpendicular to the line
+	public func normal() -> Vector {
+		//calculate base top normal
+		let baseDelta = (end - start)
+		let normal = Vector(-baseDelta.y, baseDelta.x)
 
-    /// Returns the angle of the line based on the slope
-    public func angle() -> Angle {
-        return Angle(radians: atan(slope()))
-    }
-    
-    /// Return a point at the specified distance of the line
-    /// - Parameter distance: Distance from the end point
-    public func point(at distance: Double) -> Vector {
-        var v = (end - start).normalized()
-        v *= distance
+		return normal
+	}
 
-        return start + v
-    }
+	public func slope() -> Double {
+		return (end.y - start.y) / (end.x - start.x)
+	}
 
-    ///  Linear interpolate the vector to another vector
-    /// - Parameter percent: the amount of interpolation; some value between 0.0 and 1.0
-    /// - Returns: A Vector between the original two
-    public func lerp(_ percent: Double) -> Vector {
-        Vector.lerp(percent: percent, start: start, end: end)
-    }
-    
-    public func pointOnPerimeter(_ t: Double) -> Vector {
-        Vector.lerp(percent: t, start: start, end: end)
-    }
+	/// Returns the angle of the line based on the slope
+	public func angle() -> Angle {
+		return Angle(radians: atan(slope()))
+	}
 
-    /// A Rectangle that contains the receiver
-//    public var boundingBox: Rectangle {
+	/// Return a point at the specified distance of the line
+	/// - Parameter distance: Distance from the end point
+	public func point(at distance: Double) -> Vector {
+		var v = (end - start).normalized()
+		v *= distance
 
-//        Rectangle(
-//        Rectangle(
-//            x: min(start.x, end.x),
-//            y: min(start.y, end.y),
-//            width: abs(end.x - start.x),
-//            height: abs(end.y - start.y)
-//        )
+		return start + v
+	}
 
-//    }
+	///  Linear interpolate the vector to another vector
+	/// - Parameter percent: the amount of interpolation; some value between 0.0 and 1.0
+	/// - Returns: A Vector between the original two
+	public func lerp(_ percent: Double) -> Vector {
+		Vector.lerp(percent: percent, start: start, end: end)
+	}
+
+	public func pointOnPerimeter(_ t: Double) -> Vector {
+		Vector.lerp(percent: t, start: start, end: end)
+	}
+
+	/// A Rectangle that contains the receiver
+	//    public var boundingBox: Rectangle {
+
+	//        Rectangle(
+	//        Rectangle(
+	//            x: min(start.x, end.x),
+	//            y: min(start.y, end.y),
+	//            width: abs(end.x - start.x),
+	//            height: abs(end.y - start.y)
+	//        )
+
+	//    }
 
 	/// Calculate the intersection point of a ray and a plane defined by the Line
 	/// - Parameters:
@@ -150,7 +150,7 @@ public struct Line: Shape, Drawable {
 		guard let distance = distanceToIntersection(origin: origin, dir: dir.normalized()) else {
 			return nil
 		}
-		
+
 		let norm = normal()
 		let denom = norm * dir
 
@@ -198,6 +198,7 @@ public struct Line: Shape, Drawable {
 }
 
 extension Line: RayTracable {
+
 	public func rayIntersection(_ ray: Ray) -> Vector? {
 		rayPlaneIntersection(origin: ray.origin, dir: ray.direction)
 	}
@@ -206,27 +207,24 @@ extension Line: RayTracable {
 		distanceToIntersection(origin: ray.origin, dir: ray.direction)
 	}
 
-	public func interface(of intersection: Vector) -> Line {
-		if intersection.isBehind(line: self) {
-			self
-		} else {
-			pointsSwitched()
-		}
+	public func interface(of intersection: Vector) -> Vector {
+		normal().normalized()
 	}
+
 }
 
 extension Line: SVGDrawable {
-    public func svgElement() -> XMLElement? {
-        let element = XMLElement(name: "line")
-        
-        element.addAttribute(start.x, forKey: "x1")
-        element.addAttribute(start.y, forKey: "y1")
-        element.addAttribute(end.x, forKey: "x2")
-        element.addAttribute(end.y, forKey: "y2")
+	public func svgElement() -> XMLElement? {
+		let element = XMLElement(name: "line")
 
-        element.strokeColor(Color.black)
-        element.strokeWidth(1)
+		element.addAttribute(start.x, forKey: "x1")
+		element.addAttribute(start.y, forKey: "y1")
+		element.addAttribute(end.x, forKey: "x2")
+		element.addAttribute(end.y, forKey: "y2")
 
-        return element
-    }
+		element.strokeColor(Color.black)
+		element.strokeWidth(1)
+
+		return element
+	}
 }
